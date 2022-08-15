729bb352e8c623de05dcffef35fb4af2fdb0f315
Alpha17-2 <alphaisgod1@gmail.com>
2022-08-16 00:58:19 +0530

    argument name changed

--- src/main/java/com/alpha/shopybeebackend/respositories/CartRepository.java
+++ src/main/java/com/alpha/shopybeebackend/respositories/CartRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-    public List<Cart> findAllByUserId(int user_id);
+    public List<Cart> findAllByUserId(int userId);
 }
